0605e12983a71151cb2ead5831db799738c7e434
Denis BEURIVE <beurive.denis@gmail.com>
2012-05-20 11:14:02 +0200

    Add meta data.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 
+## List of tags: http://docs.python.org/distutils/setupscript.html#meta-data
+
 setup(
     name='csvsimple',
     version='0.1.0',
@@ -7,8 +9,14 @@ setup(
     author_email='denis.beurive@gmail.com',
     py_modules=['csvsimple'],
     scripts=[],
-    url='http://pypi.python.org/pypi/CSV/',
-    license='LICENSE.txt',
-    description='Very simple CSV utility.',
+    url='http://pypi.python.org/pypi?name=csvsimple',
+    license=open('LICENCE.txt').read(),
+    description='Very simple CSV utility for Python 3.',
     long_description=open('README.txt').read(),
+    download_url='https://github.com/denis-beurive/csvsimple/zipball/master',
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+    ],
 )
